5004fb7e37b6a20311be47631101abf1cfe06aad
Ernesto Resende <dev@ernestoresende.com>
2022-11-18 12:03:21 -0300

    feat: changed from apollo-server-micro to core @apollo/server

--- apps/web/pages/api/graphql/index.ts
+++ apps/web/pages/api/graphql/index.ts
@@ -1,52 +1,20 @@
-import { NextApiResponse, NextApiRequest, PageConfig } from "next";
-import cors, { CorsOptions, CorsOptionsDelegate } from "cors";
-import { apolloServer } from '@acme/graphql';
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { NextApiResponse, NextApiRequest } from "next";
+import { schema, context } from "@acme/graphql";
 
-/* Helper method to wait for a middleware to execute before continuing
-and to throw an error when an error happens in a middleware */
-function initMiddleware(middleware: typeof cors) {
-  return (
-    req: NextApiRequest,
-    res: NextApiResponse,
-    options?: CorsOptions | CorsOptionsDelegate
-  ) =>
-    new Promise((resolve, reject) => {
-      middleware(options)(req, res, (result: Error | unknown) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
+const isDevelopment = process.env.NODE_ENV === "development";
 
-        return resolve(result);
-      });
-    });
-}
+export const apolloServer = new ApolloServer({
+  introspection: isDevelopment ? true : false,
+  csrfPrevention: isDevelopment ? true : false,
+  schema,
+});
 
-const corsMiddleware = initMiddleware(cors);
-
-const startServer = apolloServer.start();
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  await corsMiddleware(req, res, {
-    origin: "*",
-  });
-
-  if (req.method === "OPTIONS") {
-    res.end();
-    return false;
-  }
-
-  await startServer;
-
-  await apolloServer.createHandler({
-    path: "/api/graphql",
-  })(req, res);
-}
-
-export const config: PageConfig = {
-  api: {
-    bodyParser: false,
-  },
-};
+export default startServerAndCreateNextHandler(apolloServer, {
+  context: async (req: NextApiRequest, res: NextApiResponse) => ({
+    req,
+    res,
+    prisma: context.prisma,
+  }),
+});
