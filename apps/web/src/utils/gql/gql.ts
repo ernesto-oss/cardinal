ca73e64f56435bfb6005dcd97b57cf1adb325649
Ernesto Resende <dev@ernestoresende.com>
2022-11-24 09:55:33 -0300

    chore: update graphql generated artifacts

--- apps/web/src/utils/gql/gql.ts
+++ apps/web/src/utils/gql/gql.ts
@@ -2,13 +2,39 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel-plugin for production.
+ */
 const documents = {
     "\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n": types.AllPostsDocument,
 };
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
 export function graphql(source: "\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n"): (typeof documents)["\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n"];
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ *
+ *
+ * @example
+ * ```ts
+ * const query = gql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
+ * ```
+ *
+ * The query argument is unknown!
+ * Please regenerate the types.
+**/
 export function graphql(source: string): unknown;
+
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
