228c4f4a84aca1e1ac3ab140c60f60848da1eea9
Ernesto Resende <dev@ernestoresende.com>
2022-11-28 19:26:35 -0300

    chore: update @acme/tailwind-config

---
+++ packages/config/tailwind/tailwind.config.js
@@ -0,0 +1,20 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // app content
+    `src/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "eletric-pink": "#F42A8B",
+      },
+    },
+  },
+  plugins: [],
+};
