2fcacf771179ce93739b7b22853ffcf0c6b82412
Ernesto Resende <dev@ernestoresende.com>
2022-11-28 19:30:04 -0300

    feat: made tailwind work on app side

---
+++ apps/web/src/layouts/HomeLayout.tsx
@@ -0,0 +1,15 @@
+import { NavHeader } from '@acme/ui';
+
+type HomeLayoutProps = {
+  children: React.ReactNode
+}
+
+export const HomeLayout: React.FC<HomeLayoutProps> = ({ children }) => {
+  return (
+    <>
+      <NavHeader />
+      <img className="absolute top-0 -z-10" src="assets/BackgroundGradient.svg" alt="" />
+      <div className="pt-36">{children}</div>
+    </>
+  )
+}
