40206f4345431792c2acdbdb0f41a931964027e6
Ernesto Resende <dev@ernestoresende.com>
2022-12-05 03:37:45 -0300

    feat: add proper field validation and styling to auth-form

--- apps/web/src/components/user-auth-form.tsx
+++ apps/web/src/components/user-auth-form.tsx
@@ -1,41 +1,87 @@
 import * as React from "react";
 import { signIn } from "next-auth/react";
 import { clsx } from "clsx";
+import { z } from "zod";
+import { zfd } from "zod-form-data";
 
 import {
   IoLogoGithub as Github,
   IoLogoGoogle as Google,
 } from "react-icons/io5";
 
-type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>
+const checkAuthEmail = zfd.formData({
+  email: zfd.text(z.string().email()),
+});
 
-// TODO: Implement correct input field validation
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
+type ValidationErrors = z.typeToFlattenedError<z.infer<typeof checkAuthEmail>>;
+
 export const UserAuthForm: React.FC<UserAuthFormProps> = ({
   className,
   ...props
 }) => {
+  const [errors, setErrors] = React.useState<ValidationErrors>();
+  const [disableForm, setDisableForm] = React.useState<boolean>(false);
+
+  const handleEmailSignIn = (data: FormData) => {
+    const result = checkAuthEmail.safeParse(data);
+
+    if (!result.success) {
+      setErrors(result.error.flatten());
+    } else {
+      setErrors(undefined);
+      setDisableForm(true);
+      const email = data.get("email");
+      signIn("email", { email, callbackUrl: "/dashboard" });
+    }
+  };
+
+  const handleOAuthSignIn = (provider: "google" | "github") => {
+    setDisableForm(true);
+    signIn(provider, { callbackUrl: "/dashboard" });
+  };
+
   return (
     <div className={clsx(className, "pb-8")} {...props}>
-      <form className="pb-4">
+      <form
+        noValidate
+        className="pb-4"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const data = new FormData(e.currentTarget);
+          handleEmailSignIn(data);
+        }}
+      >
         {/* Form email input */}
-        <div className="flex flex-col gap-2 pb-6">
-          <label className="text-sm font-semibold" htmlFor="email">
+        <div className="flex flex-col pb-6">
+          <label className="pb-2 text-sm font-semibold" htmlFor="email">
             Email
           </label>
           <input
             id="email"
             placeholder="name@example.com"
-            className="my-0 mb-2 block h-9 w-full rounded-md border border-slate-300 py-2 px-3 text-sm placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-slate-300"
+            className="block h-9 w-full rounded-md border border-slate-300 py-2 px-3 text-sm placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-slate-300"
             type="email"
             autoCapitalize="none"
             autoComplete="email"
             autoCorrect="off"
             name="email"
+            disabled={disableForm}
           />
-          {/* TODO: Implement error message display based on validation */}
+          <div className="h-4">
+            {errors?.fieldErrors.email ? (
+              <span className="text-sm text-red-400">
+                Insert a valid email address
+              </span>
+            ) : null}
+          </div>
         </div>
         <div>
-          <button className="w-full cursor-pointer rounded-md bg-slate-800/10 py-2 px-4 text-sm font-semibold text-slate-900 transition duration-100 hover:bg-slate-800/20 focus:outline-none focus:ring-2 focus:ring-slate-300">
+          <button
+            disabled={disableForm}
+            type="submit"
+            className="w-full cursor-pointer rounded-md bg-slate-800/10 py-2 px-4 text-sm font-semibold text-slate-900 transition duration-100 hover:bg-slate-800/20 focus:outline-none focus:ring-2 focus:ring-slate-300 disabled:cursor-not-allowed disabled:hover:bg-slate-800/10 disabled:opacity-80"
+          >
             Sign In
           </button>
         </div>
@@ -52,17 +98,19 @@ export const UserAuthForm: React.FC<UserAuthFormProps> = ({
       {/* oAuth providers sign in buttons */}
       <div className="flex gap-4">
         <button
+          disabled={disableForm}
           type="button"
-          className="inline-flex w-full items-center justify-center gap-1 rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-slate-900 transition duration-100 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-300"
-          onClick={() => signIn("google")}
+          className="inline-flex w-full items-center justify-center gap-1 rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-slate-900 transition duration-100 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-300 disabled:cursor-not-allowed disabled:hover:bg-white disabled:opacity-80"
+          onClick={() => handleOAuthSignIn("google")}
         >
           <Google className="h-5 w-5" />
           Google
         </button>
         <button
+          disabled={disableForm}
           type="button"
-          className="inline-flex w-full items-center justify-center gap-1 rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-slate-900 transition duration-100 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-300"
-          onClick={() => signIn("github")}
+          className="inline-flex w-full items-center justify-center gap-1 rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-slate-900 transition duration-100 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-300 disabled:cursor-not-allowed disabled:hover:bg-white disabled:opacity-80"
+          onClick={() => handleOAuthSignIn("github")}
         >
           <Github className="h-5 w-5" />
           GitHub
