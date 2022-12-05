b69a60cf3ba7151d0733088a22e206b57a445d05
Ernesto Resende <dev@ernestoresende.com>
2022-12-05 03:36:34 -0300

    feat: added next-auth session provider

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -5,18 +5,21 @@ import { Inter } from "@next/font/google";
 
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const queryClient = new QueryClient();
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className={inter.variable}>
-        <Component {...pageProps} />
-      </div>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <div className={inter.variable}>
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
 
