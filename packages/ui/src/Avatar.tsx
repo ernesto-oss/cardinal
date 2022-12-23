599b83b26cfd703a71ee9c3e7400ea09183952fc
Ernesto Resende <dev@ernestoresende.com>
2022-12-23 20:11:34 -0300

    chore: update Avatar component layout

--- packages/ui/src/Avatar.tsx
+++ packages/ui/src/Avatar.tsx
@@ -14,14 +14,14 @@ export const Avatar: React.FC<AvatarProps> = ({
 }) => {
 
   return (
-    <AvatarComponent.Root className="inline-flex h-9 w-9 select-none items-center justify-center overflow-hidden rounded-full bg-slate-500 align-middle">
+    <AvatarComponent.Root className="inline-flex h-9 w-9 select-none items-center justify-center overflow-hidden rounded-full bg-transparent align-middle">
       <AvatarComponent.Image
         className="h-full w-full rounded-full object-cover"
         src={image as string}
         alt={imageAlt}
       />
       <AvatarComponent.Fallback
-        className="flex h-full w-full items-center justify-center bg-slate-300 text-base font-bold text-slate-900"
+        className="flex h-full w-full items-center justify-center bg-slate-300/50 text-base font-bold text-slate-700"
       >
         {fallback}
       </AvatarComponent.Fallback>
