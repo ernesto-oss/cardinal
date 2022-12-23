c2ec1c5e957e62f675700b93ef01e71cf7d6481d
Ernesto Resende <dev@ernestoresende.com>
2022-12-23 20:11:09 -0300

    fix: remove unused exports from tailwind config

--- packages/config/tailwind/tailwind.config.js
+++ packages/config/tailwind/tailwind.config.js
@@ -1,6 +1,3 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
