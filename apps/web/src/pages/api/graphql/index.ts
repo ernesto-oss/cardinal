78b0e98f5668eba288bf0bcf9a249d5b35a7dab9
Ernesto Resende <dev@ernestoresende.com>
2023-01-25 06:16:37 -0300

    fix: rename `useReponseCache` on import to prevent build error

--- apps/web/src/pages/api/graphql/index.ts
+++ apps/web/src/pages/api/graphql/index.ts
@@ -1,17 +1,22 @@
 import { createYoga } from "graphql-yoga";
-import { useResponseCache } from "@graphql-yoga/plugin-response-cache";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { schema } from "@acme/graphql";
 import { getServerAuthSession } from "@acme/auth";
 import { parse } from "cookie";
 
+/**
+ * Rename the import because Next.js lint rules thinks anything that starts with `use`
+ * is a React hook and will throw an error during build.
+ */
+import { useResponseCache as responseCache } from "@graphql-yoga/plugin-response-cache";
+
+/**
+ * Disabling the body parsing is required to allow Yoga to handle the request body.
+ * Specially useful if you're dealing with file uploads from GraphQL.
+ * @see: https://the-guild.dev/graphql/yoga-server/docs/features/file-uploads
+ */
 export const config = {
   api: {
-    /**
-     * Disabling the body parsing is required to allow Yoga to handle the request body. Specially useful
-     * if you're dealing with file uploads from GraphQL.
-     * @see: https://the-guild.dev/graphql/yoga-server/docs/features/file-uploads
-     */
     bodyParser: false,
   },
 };
@@ -33,10 +38,10 @@ export default createYoga<{
   schema,
   plugins: [
     /**
-     * Session based in-memory response caching. We extract the session token value from the request
-     * and use it as the cache key when existent.
+     * Session based in-memory response caching. We extract the session token value
+     * from the request and use it as the cache key when existent.
      */
-    useResponseCache({
+    responseCache({
       session: (request) => {
         const cookies = parse(request.headers.get("cookie") || "");
         const sessionToken = cookies["next-auth.session-token"];
@@ -45,7 +50,8 @@ export default createYoga<{
     }),
   ],
   /**
-   * Needs to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-   * */
+   * Needs to be defined explicitly because our endpoint lives at a different
+   * path other than `/graphql`
+   */
   graphqlEndpoint: "/api/graphql",
 });
