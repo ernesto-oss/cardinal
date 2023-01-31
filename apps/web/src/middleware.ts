f928fd22ec707db0763622309a57e7e32a3ce7bf
Ernesto Resende <dev@ernestoresende.com>
2023-01-31 16:12:28 -0300

    docs: properly document middleware conditions

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,10 +1,31 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+/**
+ * We use middlewares primarily to redirect users to the correct page based on their
+ * authentication status.
+ */
 export function middleware(request: NextRequest) {
   const authenticationCookie = request.cookies.has("next-auth.session-token");
+
+  /**
+   * Authenticated user is redirected to dashboard upon trying to request the signIn
+   * page.
+   */
   if (request.nextUrl.pathname === "/auth/signin" && authenticationCookie) {
     const pathToRedirect = new URL("/dashboard/posts", request.url);
+    return NextResponse.redirect(pathToRedirect);
+  }
+
+  /**
+   * Authenticated user is redirected to dashboard upon trying to request email
+   * verification page.
+   */
+  if (
+    request.nextUrl.pathname === "/auth/verify-request" &&
+    authenticationCookie
+  ) {
+    const pathToRedirect = new URL("/", request.url);
     return NextResponse.redirect(pathToRedirect);
   }
 }
