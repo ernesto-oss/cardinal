092c94c4c10d1b4eb559cfec2436fca43d6cd513
Ernesto Resende <dev@ernestoresende.com>
2023-02-17 15:14:10 -0300

    chore: remove unstable prefix from getServerSession

--- packages/auth/src/get-session.ts
+++ packages/auth/src/get-session.ts
@@ -1,13 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "./auth-options";
 
 /**
  * Wrapper for the `unstable_getServerSession` function from `next-auth` used on
  * GraphQL request context and on restricted API Routes.
- * 
- * The `unstable` prefix is used because it may change on future versions of next-auth,
- * but it's safe to use.
  * 
  * @see: https://next-auth.js.org/configuration/nextjs
  */
@@ -15,5 +12,5 @@ export const getServerAuthSession = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  return await unstable_getServerSession(req, res, authOptions);
+  return await getServerSession(req, res, authOptions);
 };
