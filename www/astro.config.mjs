ac5d33309a7d95887547306d9d97149d8e1ff8f4
Ernesto Resende <dev@ernestoresende.com>
2023-02-24 06:58:02 -0300

    chore: remove preact as astro integration

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), tailwind(), image({
-    serviceEntryPoint:  "@astrojs/image/sharp"
-  })],
-  site: `https://astro.build`
+    react(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  site: `https://astro.build`,
 });
