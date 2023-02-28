e407215bfa1fee968462080e33b1b2d8e528155d
Ernesto Resende <dev@ernestoresende.com>
2023-02-28 06:04:19 -0300

    feat: add prettier config to cli

---
+++ cli/.prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: "always",
+  printWidth: 120,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+}
