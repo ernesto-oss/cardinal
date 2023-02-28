f97d4c7cd29d872ec36b3e14af0a56a4d9aa3e98
Ernesto Resende <dev@ernestoresende.com>
2023-02-28 06:12:13 -0300

    feat: update cli checks & prompts

--- cli/src/cli/index.ts
+++ cli/src/cli/index.ts
@@ -1,20 +1,21 @@
-import { select, multiselect, spinner, text } from "@clack/prompts";
-import { setTimeout as sleep } from "node:timers/promises";
+import { select, multiselect, text } from "@clack/prompts";
+import colors from "picocolors";
 
 import {
-  validateAppName,
+  validateAppDirectory,
   checkValidAddons,
   checkValidFrontendDeployProviders,
   checkValidGraphqlAgents,
 } from "@/utils/checks";
 import type { FrontendFramework } from "@/utils/checks";
 
-export const promptAppName = async () => {
+export const promptAppDirectory = async () => {
   const name = await text({
-    message: "What should we name your new project?",
-    placeholder: "my-cardinal-app",
+    message: "Where should we create your project?",
+    placeholder: "./my-cardinal-app",
+    defaultValue: "./my-cardinal-app",
     validate(value) {
-      return validateAppName(value);
+      return validateAppDirectory(value);
     },
   });
   return name;
@@ -32,12 +33,8 @@ export const promptFrontendFramework = async () => {
   return frontendFramework;
 };
 
-export const promptDeployProvider = async (
-  frontendFramework: FrontendFramework
-) => {
-  const validFrontendDeploys = await checkValidFrontendDeployProviders(
-    frontendFramework
-  );
+export const promptDeployProvider = async (frontendFramework: FrontendFramework) => {
+  const validFrontendDeploys = await checkValidFrontendDeployProviders(frontendFramework);
   const deployProvider = await select({
     message: "Where do you want to deploy the front-end application?",
     options: validFrontendDeploys,
@@ -45,9 +42,7 @@ export const promptDeployProvider = async (
   return deployProvider;
 };
 
-export const promptGraphqlHostingAgent = async (
-  frontendFramework: FrontendFramework
-) => {
+export const promptGraphqlHostingAgent = async (frontendFramework: FrontendFramework) => {
   const validGraphQlAgents = await checkValidGraphqlAgents(frontendFramework);
   const graphQLHostingAgent = await select({
     message: "How do you want to host your GraphQL API?",
@@ -56,20 +51,14 @@ export const promptGraphqlHostingAgent = async (
   return graphQLHostingAgent;
 };
 
-export const promptAdditionalPackages = async (
-  frontendFrameword: FrontendFramework
-) => {
+export const promptAdditionalPackages = async (frontendFrameword: FrontendFramework) => {
   const validAddons = await checkValidAddons(frontendFrameword);
   const addons = await multiselect({
-    message: "Which additions would you like to enable?",
+    message: `Which additions would you like to enable? (Press ${colors.cyan("<space>")} to select and ${colors.cyan(
+      "<enter>",
+    )} to proceed)`,
     options: validAddons,
+    required: false,
   });
   return addons;
-};
-
-export const scaffoldProject = async () => {
-  const s = spinner();
-  s.start("Scaffolding your project");
-  await sleep(3000);
-  s.stop("✅ Success!") 
 };
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,33 +1,102 @@
 #!/usr/bin/env node
-
-import { intro, outro } from "@clack/prompts";
+import { intro, outro, group, cancel } from "@clack/prompts";
 import { setTimeout as sleep } from "node:timers/promises";
 import color from "picocolors";
-
+import { getUserPkgManager } from "@/utils/getUserPackageManager.js";
+import { parseNameAndPath } from "@/utils/parseNameAndPath.js";
 import {
-  promptAppName,
+  promptAppDirectory,
   promptFrontendFramework,
   promptDeployProvider,
   promptGraphqlHostingAgent,
   promptAdditionalPackages,
-  scaffoldProject,
-} from "@/cli";
+} from "@/cli/index.js";
+import { type AvailablePackages } from "@/installers/index.js";
+import { createProject } from "@/helpers/createProject.js";
+
+export const runCli = async () => {
+  const corePromptGroup = await group(
+    {
+      appDir: () => promptAppDirectory(),
+      frontendFramework: () => promptFrontendFramework(),
+    },
+    {
+      onCancel: () => {
+        cancel("Installation stopped. Come back when you're ready to try again.");
+        process.exit(0);
+      },
+    },
+  );
+
+  const frontendFramework = corePromptGroup.frontendFramework;
+
+  const conditionalPromptGroup = await group(
+    {
+      deployProvider: () => promptDeployProvider(frontendFramework),
+      graphqlHostingAgent: () => promptGraphqlHostingAgent(frontendFramework),
+      additionalPackages: () => promptAdditionalPackages(frontendFramework),
+    },
+    {
+      onCancel: () => {
+        cancel("Installation stopped. Come back when you're ready to try again");
+        process.exit(0);
+      },
+    },
+  );
+
+  const { deployProvider, graphqlHostingAgent, additionalPackages } = conditionalPromptGroup;
+
+  const addons = additionalPackages as AvailablePackages[];
+
+  return {
+    appDir: corePromptGroup.appDir,
+    frontendFramework: frontendFramework,
+    deployProvider: deployProvider,
+    graphQLHostingAgent: graphqlHostingAgent,
+    additionalPackages: addons,
+  };
+};
 
 async function main() {
-  console.log();
-  intro(color.inverse("create cardinal-app"));
+  intro(`Let's create your new monorepo project with ${color.bold(color.magenta("Cardinal"))} ✨`);
 
-  const appName = await promptAppName();
-  const frontendFramework = await promptFrontendFramework();
-  const deployProvider = await promptDeployProvider(frontendFramework);
-  const graphQLHostingAgent = await promptGraphqlHostingAgent(
-    frontendFramework
-  );
-  const additionalPackages = await promptAdditionalPackages(frontendFramework);
-  const scaffold = await scaffoldProject();
+  /* Get user package manager */
+  const pkgManager = getUserPkgManager();
 
-  outro("You're all set!");
+  /* Run CLI prompts and get information about what packages will be included on the scaffolded project */
+  const { appDir, additionalPackages, deployProvider, frontendFramework, graphQLHostingAgent } = await runCli();
+
+  /* Parses the app name and directory from the user input */
+  const [scopedAppName, appPath] = parseNameAndPath(appDir);
+
+  /* Bootstraps all the template files into the project directory */
+  await createProject({
+    packages: additionalPackages,
+    pkgManager,
+    projectDir: appPath,
+    projectName: scopedAppName,
+  });
+
+  outro(`
+    ${color.bold(color.green("✅Success!"))} Project created in ${color.magenta(appPath)}
+    Next steps:
+    ${color.dim(`
+    cd ${appPath}
+    ${pkgManager} start
+    `)}
+  `);
+
+  /* Small time-gap after showing the final success message so the return to regular terminal control doesn't look too jarring */
   await sleep(1000);
 }
 
-main().catch(console.error);
+main().catch((err) => {
+  color.red(color.bold("The installation ran into an error:"));
+  if (err instanceof Error) {
+    console.error(err);
+  } else {
+    color.red(color.bold("An unknown error has ocurred. Please open an issue on GitHub with the issue bellow:"));
+    console.log(err);
+  }
+  process.exit(1);
+});
