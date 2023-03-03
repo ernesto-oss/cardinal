ca2c2367c88a36ad4b84ca2a49ae8d80d2b922d5
Ernesto Resende <dev@ernestoresende.com>
2023-03-03 06:47:50 -0300

    chore: move graphql generated artifacts

--- apps/web/src/generated/graphql/gql.ts
+++ apps/web/src/generated/graphql/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from "./graphql";
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,16 +13,8 @@ import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel-plugin for production.
  */
 const documents = {
-  "\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n":
-    types.AllPostsDocument,
+    "\n  query GetMyUser {\n    myUser {\n      __typename\n      id\n      name\n      email\n    }\n  }\n": types.GetMyUserDocument,
 };
-
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function graphql(
-  source: "\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n",
-): typeof documents["\n  query allPosts {\n    posts {\n      id\n      title\n      content\n    }\n  }\n"];
 
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -35,12 +27,16 @@ export function graphql(
  *
  * The query argument is unknown!
  * Please regenerate the types.
- **/
+ */
 export function graphql(source: string): unknown;
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query GetMyUser {\n    myUser {\n      __typename\n      id\n      name\n      email\n    }\n  }\n"): (typeof documents)["\n  query GetMyUser {\n    myUser {\n      __typename\n      id\n      name\n      email\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
