5843a9746f9a7c391e0f10985f81e39164d147ca
Ernesto Resende <dev@ernestoresende.com>
2023-03-05 15:21:43 -0300

    fix: add prisma plugin to next.config
    recent next update broke the handling of Prisma Client
    (see: https://github.com/vercel/next.js/issues/46070)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,14 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import("next").NextConfig} */
 const config = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ["@acme/database", "@acme/graphql", "@acme/auth"],
