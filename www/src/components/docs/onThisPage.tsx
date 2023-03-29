621e36af71ed271e403be82ad7b7c921ce1a1964
Ernesto Resende <dev@ernestoresende.com>
2023-03-29 15:39:42 -0300

    feat: add onThisPage for mobile view

---
+++ www/src/components/docs/onThisPage.tsx
@@ -0,0 +1,43 @@
+import type { MarkdownHeading } from "astro";
+import { useState, useEffect } from "react";
+import { ChevronsUpDown } from "lucide-react";
+
+import TableOfContents from "@/components/docs/tableOfContents";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/docs/collapsible";
+
+export const OnThisPage: React.FC<{ headings: MarkdownHeading[] }> = ({ headings }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+    return () => {
+      setMounted(false);
+    };
+  }, []);
+
+  if (!mounted)
+    return (
+      <button className="flex items-center gap-2 md:px-8 lg:hidden">
+        <span className="text-default font-semibold text-pink-600 dark:text-pink-500">On this page</span>
+        <ChevronsUpDown className="h-5 w-5 text-pink-600 dark:text-pink-500" />
+      </button>
+    );
+
+  return (
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="w-full pb-8 lg:hidden">
+      <CollapsibleTrigger asChild>
+        <button className="flex items-center gap-2 md:px-8">
+          <span className="text-default font-semibold text-pink-600 dark:text-pink-500">On this page</span>
+          <ChevronsUpDown className="h-5 w-5 text-pink-600 dark:text-pink-500" />
+        </button>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <div className="pt-8">
+          <TableOfContents headings={headings} />
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+};
+
+export default OnThisPage;
