c99bb4d8559da477104ce6e6fd87347f323a1f7b
Ernesto Resende <dev@ernestoresende.com>
2023-04-05 22:24:15 -0300

    chore: update web package configs

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,14 @@
+import * as path from 'path';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: { port: 3000 },
   plugins: [react()],
 });
