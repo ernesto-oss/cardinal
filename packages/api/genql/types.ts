ec67ba654c328bf9571a04d5ea72251120aa2d88
Ernesto Resende <dev@ernestoresende.com>
2023-04-24 01:03:11 -0300

    chore: update `api` generated artifacts

---
+++ packages/api/genql/types.ts
@@ -0,0 +1,23 @@
+export default {
+    "scalars": [
+        0,
+        2,
+        3
+    ],
+    "types": {
+        "DateTime": {},
+        "Query": {
+            "greeting": [
+                2
+            ],
+            "somethingElse": [
+                2
+            ],
+            "__typename": [
+                2
+            ]
+        },
+        "String": {},
+        "Boolean": {}
+    }
+}
