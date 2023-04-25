4009bb755220489ac780bac350f98ccdc8ff07ff
Ernesto Resende <dev@ernestoresende.com>
2023-04-24 22:34:12 -0300

    feat: update `utils` with genql client

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import "@/styles/globals.css";
 
-import { Provider as UrqlProvider } from "urql";
-import { urqlClient } from "@/utils/urql";
-
 import type { ReactElement, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { Inter } from "next/font/google";
 
 export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,7 +14,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
@@ -27,14 +24,9 @@ export default function App({
 
   return getLayout(
     <SessionProvider session={session}>
-      <UrqlProvider value={urqlClient}>
-        <style jsx global>{`
-          html {
-            font-family: ${inter.style.fontFamily};
-          }
-        `}</style>
+      <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
-      </UrqlProvider>
+      </QueryClientProvider>
     </SessionProvider>,
   );
 }
