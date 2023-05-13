db40f62d800f665214b86168cf4d80192d7b6885
Ernesto Resende <dev@ernestoresende.com>
2023-05-13 00:54:13 -0300

    feat(#11): introduce lucia-auth config `@acme/auth`

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,7 +1,49 @@
-export { authOptions } from "./src/auth-options";
-export { getServerAuthSession } from "./src/get-session";
+import type { Session } from 'lucia-auth'
+import lucia from "lucia-auth";
+import { web } from "lucia-auth/middleware";
+import { sign, decode } from 'jsonwebtoken';
+import { env } from '@acme/config/env'
 
-import type { Session as NextAuthSession } from "next-auth";
+/* Prisma client and adapter */
+import prismaAdapter from "@lucia-auth/adapter-prisma";
+import { prisma as db } from "@acme/database";
 
-type SessionWithId = { user?: { id?: string | null } };
-export type Session = NextAuthSession & SessionWithId;
+/* Crypto pollyfill for Node.js 18 and bellow.
+ * You can remove it if you're using a recent version of Node
+ * that doesn´t need to be polyfilled */
+import "lucia-auth/polyfill/node";
+
+const authSecret = env.AUTH_SECRET;    
+
+export const auth = lucia({
+  adapter: prismaAdapter(db as any),
+  env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
+  middleware: web(),
+  transformDatabaseUser: (userData) => {
+    return {
+      userId: userData.id,
+      email: userData.email,
+    };
+  },
+});
+
+export const signJwtToken = (session: Session) => {
+  const { sessionId, activePeriodExpiresAt, state } = session; 
+  const tokenPayload = { sessionId, expires: activePeriodExpiresAt, state }
+
+  return sign(tokenPayload, authSecret)
+}
+
+export const decodeJwtToken = (token: string) => {
+  return decode(token);
+}
+
+export type JwtPayload = {
+  sessionId: string;
+  expires: string;
+  state: "idle" | "active";
+  iat: number;
+};
+export type Auth = typeof auth;
+export type { Session } from 'lucia-auth'
+export { LuciaError } from 'lucia-auth';
