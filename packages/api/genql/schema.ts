7bdd3667e5a62824687659eefa1723677a9904bd
Ernesto Resende <dev@ernestoresende.com>
2023-05-20 00:39:50 -0300

    chore: update `@acme/api` codegen artifacts

--- packages/api/genql/schema.ts
+++ packages/api/genql/schema.ts
@@ -1,69 +1,22 @@
 // @ts-nocheck
 export type Scalars = {
-    ID: string,
-    String: string,
     DateTime: any,
+    String: string,
     Boolean: boolean,
 }
 
-export interface Article {
-    id: Scalars['ID']
-    title: Scalars['String']
-    url: Scalars['String']
-    __typename: 'Article'
-}
-
-export interface Mutation {
-    /** Creates and returns a new Article */
-    createArticle: Article
-    __typename: 'Mutation'
-}
-
 export interface Query {
-    article?: Article
-    articles: Article[]
     greeting: Scalars['String']
+    protected: Scalars['String']
     __typename: 'Query'
 }
 
-export interface ArticleGenqlSelection{
-    id?: boolean | number
-    title?: boolean | number
-    url?: boolean | number
-    __typename?: boolean | number
-    __scalar?: boolean | number
-}
-
-export interface MutationGenqlSelection{
-    /** Creates and returns a new Article */
-    createArticle?: (ArticleGenqlSelection & { __args: {title: Scalars['String'], url: Scalars['String']} })
-    __typename?: boolean | number
-    __scalar?: boolean | number
-}
-
 export interface QueryGenqlSelection{
-    article?: (ArticleGenqlSelection & { __args: {articleID: Scalars['String']} })
-    articles?: ArticleGenqlSelection
     greeting?: boolean | number
+    protected?: boolean | number
     __typename?: boolean | number
     __scalar?: boolean | number
 }
-
-
-    const Article_possibleTypes: string[] = ['Article']
-    export const isArticle = (obj?: { __typename?: any } | null): obj is Article => {
-      if (!obj?.__typename) throw new Error('__typename is missing in "isArticle"')
-      return Article_possibleTypes.includes(obj.__typename)
-    }
-    
-
-
-    const Mutation_possibleTypes: string[] = ['Mutation']
-    export const isMutation = (obj?: { __typename?: any } | null): obj is Mutation => {
-      if (!obj?.__typename) throw new Error('__typename is missing in "isMutation"')
-      return Mutation_possibleTypes.includes(obj.__typename)
-    }
-    
 
 
     const Query_possibleTypes: string[] = ['Query']
