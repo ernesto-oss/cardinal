2225b26b33ade4700cc09cf43f23218345db42bf
Ernesto Resende <dev@ernestoresende.com>
2023-05-26 17:03:43 -0300

    fix: add type overrides for `@clack/prompts`

---
+++ cli/src/types/index.d.ts
@@ -0,0 +1,18 @@
+export type FrontendFramework = "next" | "react";
+export type BackendType = "rest" | "trpc" | "graphql" | "rsc";
+export type DatabaseProvider = "planetscale" | "mysql" | "postgres" | "sqlite" | "none";
+type DeploymentProvider = "aws" | "vercel";
+
+export type Primitive = Readonly<FrontendFramework | BackendType | DatabaseProvider | DeploymentProvider>;
+
+export interface Option<Value extends Primitive> {
+  value: Value;
+  label: string;
+  hint?: string;
+}
+
+export interface SelectOptions<Options extends Option<Value>[], Value extends Option> {
+  message: string;
+  options: Options;
+  initialValue?: Value;
+}
