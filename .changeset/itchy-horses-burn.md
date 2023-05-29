0f4b155974e70589510a140097de4add1be705fa
Ernesto Resende <dev@ernestoresende.com>
2023-05-29 18:19:30 -0300

    chore: reset  directory

---
+++ .changeset/itchy-horses-burn.md
@@ -0,0 +1,5 @@
+---
+"create-cardinal-app": patch
+---
+
+(cli) reset `/templates` directory to rebuild template structure for the cli app
--- cli/template/addons/auth/index.ts
+++
@@ -1,7 +0,0 @@
-export { authOptions } from "./src/auth-options";
-export { getServerAuthSession } from "./src/get-session";
-
-import type { Session as NextAuthSession } from "next-auth";
-
-type SessionWithId = { user?: { id?: string | null } };
-export type Session = NextAuthSession & SessionWithId;
