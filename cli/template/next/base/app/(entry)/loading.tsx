934bfeaf53ff5f115c46d3450dc00f1ad561db56
Ernesto Resende <dev@ernestoresende.com>
2023-06-05 13:42:19 -0300

    chore: update dependencies, scripts and tsconfig across workspace

---
+++ cli/template/next/base/app/(entry)/loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from '@/components/ui/spinner';
+
+export default function Loading() {
+  return (
+    <div className="flex w-full flex-col items-center justify-between">
+      <Spinner />
+    </div>
+  );
+}
+
