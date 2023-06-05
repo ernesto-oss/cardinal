5298f32f417d629a32aa71825d0295a7da12324e
Ernesto Resende <dev@ernestoresende.com>
2023-06-05 13:53:18 -0300

    fix: packageManagerPrefix replacer

--- cli/src/helpers/createPackageScripts.ts
+++ cli/src/helpers/createPackageScripts.ts
@@ -12,22 +12,22 @@ export const graphQLScriptsMap = {
   /* Core scripts used by graphql-codegen */
   "codegen:schema": "graphql-codegen --config codegen.ts",
   "codegen:client": "npx @genql/cli --output ./genql --schema ./schema.graphql --esm",
-  codegen: "codegen:schema && codegen:client",
+  codegen: "<pkgCommand> codegen:schema && <pkgCommand> codegen:client",
   dev: 'chokidar "**/*.ts" --silent --initial -i "node_modules" -i "genql" -c "<pkgCommand> codegen"',
 };
 
 export const databaseScriptsMap = {
   /* Core scripts used by `drizzle-kit` operations */
-  "db-migration": "with-env drizzle-kit generate",
-  "db-introspect": "with-env drizzle-kit introspect",
-  "db-drop": "with-env drizzle-kit drop out=drizzle",
+  "db-migration": "<pkgCommand> with-env drizzle-kit generate",
+  "db-introspect": "<pkgCommand> with-env drizzle-kit introspect",
+  "db-drop": "<pkgCommand> with-env drizzle-kit drop out=drizzle",
 };
 
 export const nextScriptsMap = {
   /* Core scripts used by `next` */
-  dev: "with-env next dev",
-  build: "with-env next build",
-  start: "with-env next start",
+  dev: "<pkgCommand> with-env next dev",
+  build: "<pkgCommand> with-env next build",
+  start: "<pkgCommand> with-env next start",
   lint: "next lint",
 };
 
@@ -50,8 +50,9 @@ export function createPackageScripts<T extends string>(opts: {
   scripts.forEach((scriptKey) => {
     const scriptCommand = scriptsMap[scriptKey];
 
-    const packageManagerPrefix = packageManager === "npm" ? "npm run" : packageManager === "yarn" ? "yarn" : "pnpm";
-    const formattedScriptCommand = scriptCommand.replace("<pkgCommand>", packageManagerPrefix);
+    // const packageManagerPrefix = packageManager === "npm" ? "npm run" : packageManager === "yarn" ? "yarn" : "pnpm";
+    const packageManagerPrefix = "pnpm";
+    const formattedScriptCommand = scriptCommand.replace(/<pkgCommand>/g, packageManagerPrefix);
 
     packageJson.scripts![scriptKey] = `${formattedScriptCommand}${commandSuffix ? `:${commandSuffix}` : ""}`;
   });
