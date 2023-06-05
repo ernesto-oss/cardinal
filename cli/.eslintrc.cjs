59271c71044767bd2fd46d8aeaa8bdfb72fae0bf
Ernesto Resende <dev@ernestoresende.com>
2023-06-05 13:54:20 -0300

    chore: add basic eslint config to `cli`

---
+++ cli/.eslintrc.cjs
@@ -0,0 +1,25 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["import"],
+  ignorePatterns: ["**/template/*", "**/dist/*"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+  },
+};
+
+module.exports = config;
