4e4f26af188995071fdc70f7550a6f17c12f5c16
Ernesto Resende <dev@ernestoresende.com>
2023-06-09 15:54:54 -0300

    refactor: overhaul dependency maps

--- cli/src/helpers/addPackageDependency.ts
+++ cli/src/helpers/addPackageDependency.ts
@@ -1,83 +1,24 @@
 import { type PackageJson } from "type-fest";
 
-export const graphqlDependencyMap = {
-  "@acme/auth": "workspace:*",
-  "@acme/database": "workspace:*",
-  "@pothos/core": "^3.30.0",
-  "@pothos/plugin-scope-auth": "^3.19.2",
-  graphql: "^16.6.0",
-  "graphql-yoga": "^3.9.1",
+import authDependencyMapJson from "./dependencyMaps/authDependencyMap.json" assert { type: "json" };
+import coreDependencyMapJson from "./dependencyMaps/coreDependencyMap.json" assert { type: "json" };
+import databaseDependencyMapJson from "./dependencyMaps/databaseDependencyMaps.json" assert { type: "json" };
+import graphqlDependencyMapJson from "./dependencyMaps/graphqlDependencyMap.json" assert { type: "json" };
+import nextDependencyMapJson from "./dependencyMaps/nextDependencyMap.json" assert { type: "json" };
+import { type DependencyMap } from "@/types/index.js";
 
-  /* Code generation */
-  "@genql/cli": "3.0.5",
-  "@graphql-codegen/cli": "^3.3.1",
-  "@graphql-codegen/schema-ast": "^3.0.1",
-  react: "^18.2.0",
-  "@types/react": "^18.2.7",
+export const coreDependencyMap = coreDependencyMapJson;
+export const graphqlDependencyMap = graphqlDependencyMapJson;
+export const databaseDependencyMap = databaseDependencyMapJson;
+export const authDependencyMap = authDependencyMapJson;
+export const nextjsDependencyMap = nextDependencyMapJson;
 
-  /* Dev dependencies */
-  "@types/node": "^20.2.5",
-  "chokidar-cli": "^3.0.0",
-  next: "13.4.4",
-};
-
-export const databaseDependencyMap = {
-  "@acme/config": "workspace:*",
-  "drizzle-orm": "^0.26.3",
-  "drizzle-kit": "^0.18.1",
-
-  /* Adapters */
-  "@planetscale/database": "~1.7.0",
-};
-
-export const authDependencyMap = {
-  "@acme/config": "workspace:*",
-  "@acme/database": "workspace:*",
-  zod: "^3.21.0",
-  "lucia-auth": "^1.7.0",
-
-  /* Adapters */
-  "@lucia-auth/adapter-mysql": "^1.1.1",
-
-  /* Dev dependencies */
-  next: "13.4.4",
-};
-
-export const nextjsDependencyMap = {
-  "@acme/api": "workspace:*",
-  "@acme/auth": "workspace:*",
-  "@acme/config": "workspace:*",
-  "@hookform/resolvers": "^3.1.0",
-  "@radix-ui/react-label": "^2.0.2",
-  autoprefixer: "^10.4.14",
-  clsx: "^1.2.1",
-  graphql: "^16.6.0",
-  next: "13.4.4",
-  postcss: "^8.4.23",
-  react: "^18.2.0",
-  "react-dom": "^18.2.0",
-  "react-hook-form": "^7.44.1",
-  "react-icons": "^4.8.0",
-  tailwindcss: "^3.3.2",
-  zod: "^3.21.4",
-
-  // Dev dependencies
-  "@types/node": "^20.2.5",
-  "@types/react": "^18.2.7",
-  "@types/react-dom": "^18.2.4",
-  "dotenv-cli": "^7.2.1",
-  eslint: "8.41.0",
-  "eslint-config-next": "13.4.4",
-};
-
-type DependencyMap = {
-  [key: string]: string;
-};
-
-export type AvailableDatabaseDependenciesKeys = keyof typeof databaseDependencyMap;
+export type AvailableDatabaseDependenciesKeys =
+  keyof typeof databaseDependencyMap;
 export type AvailableAuthDependenciesKeys = keyof typeof authDependencyMap;
 export type AvailableNextjsDependenciesKeys = keyof typeof nextjsDependencyMap;
-export type AvailableGraphqlDependenciesKeys = keyof typeof graphqlDependencyMap;
+export type AvailableGraphqlDependenciesKeys =
+  keyof typeof graphqlDependencyMap;
 export type AvailableDependenciesType = Readonly<
   | AvailableDatabaseDependenciesKeys
   | AvailableAuthDependenciesKeys
@@ -91,7 +32,9 @@ export type AvailableDependencies<T extends AvailableDependenciesType> = T[];
  * Inserts the dependencies from a given set of `dependencyMap`
  * into a given `package.json`
  */
-export function addPackageDependency<T extends AvailableDependenciesType>(opts: {
+export function addPackageDependency<
+  T extends AvailableDependenciesType,
+>(opts: {
   dependencyMap: DependencyMap;
   dependencies: AvailableDependencies<T>;
   devDependency: boolean;
