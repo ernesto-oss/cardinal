97daacba2032c29d0693c5a3063634eaf214cb08
Ernesto Resende <dev@ernestoresende.com>
2023-06-10 18:45:59 -0300

    chore: move environment variable injection to tsup script

--- cli/tsup.config.ts
+++ cli/tsup.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "tsup";
 const isDev = process.env.npm_lifecycle_event === "dev";
 
 export default defineConfig({
-  env: {
-    NODE_ENV: process.env.NODE_ENV,
-    PACKAGE_MANAGER_OVERRIDE: process.env.PACKAGE_MANAGER_OVERRIDE as string,
-  },
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
