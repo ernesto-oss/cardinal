32e20c6e62542ced474d0e1b877ecc0f66e64510
Ernesto Resende <dev@ernestoresende.com>
2023-06-22 00:04:54 -0300

    fix: match pagination with refactored sidebar (#20)

--- www/src/utils/pagination.ts
+++ www/src/utils/pagination.ts
@@ -1,12 +1,37 @@
-import { SIDEBAR, type Sidebar, type SidebarItem, type SidebarItemLink } from "@/consts";
+import { sidebar, type SidebarItem } from "@/utils/sidebar";
 
-export function paginate(lang: keyof Sidebar, path: SidebarItemLink) {
-  const routes = Object.values(SIDEBAR[lang]).flat() as SidebarItem[];
-  const index = routes.map((item) => item.link).indexOf(path);
+export function paginate(path: string) {
+  const routes = Object.values(sidebar).flat() as SidebarItem[];
+  const flattenedRoutes = [] as SidebarItem[];
 
+  /** Flatten nested routes into a single array containing all the routes.
+   * This is not very portable until we manage to limit the depth of childItems
+   * in the `sidebar` variable
+   */
+  routes.forEach((headerItem) => {
+    const childItems = headerItem.items;
+    const childItemContainsLink = childItems?.filter((item) =>
+      item.link.includes("docs"),
+    );
+    const childItemContainsItems = childItems?.filter((item) => item.items);
+
+    if (childItemContainsLink) {
+      childItemContainsLink.forEach((item) => {
+        flattenedRoutes.push(item);
+      });
+    }
+
+    if (childItemContainsItems) {
+      childItemContainsItems.forEach((item) => {
+        item.items?.forEach((item) => flattenedRoutes.push(item));
+      });
+    }
+  });
+
+  const index = flattenedRoutes.map((item) => item.link).indexOf(path);
   if (index === -1) return { prev: undefined, next: undefined };
-
-  const prev = index > 0 ? routes[index - 1] : undefined;
-  const next = index < routes.length - 1 ? routes[index + 1] : undefined;
+  const prev = index > 0 ? flattenedRoutes[index - 1] : undefined;
+  const next =
+    index < flattenedRoutes.length - 1 ? flattenedRoutes[index + 1] : undefined;
   return { prev, next };
 }
